1d99a2ed0b3d8d4d7840c925b5f4e507883706bc
Rahul Singh Bisht <161435800+RahulSGits@users.noreply.github.com>
2025-11-09 17:29:09 +0530

    Update API base URL to production server

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://localhost:5001/api';
+// const API_BASE_URL = 'http://localhost:5001/api';
+
+const API_BASE_URL = 'https://careerconnect-backend.onrender.com/api';
 
 // DOM Elements
 const navbar = document.querySelector('.navbar');
